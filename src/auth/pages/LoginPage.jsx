0f8c9b64be9f5074fa533a84eca50491140f08d0
Diegantos <diegantos8@gmail.com>
2023-07-24 11:37:27 +0200

    login

---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,25 @@
+
+export const LoginPage = () => {
+  return (
+    <div className="modal" tabindex="-1">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Login Page</h5>
+          </div>
+          <form>
+            <div className="modal-body">
+                <input className="form-control my-3 w-75" placeholder="Username" name="username" />
+                <input className="form-control my-3 w-75" placeholder="Password" name="password" />
+            </div>
+            <div className="modal-footer">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
