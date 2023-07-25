789b54856cfa14118943448ac96fe9f4e3d4fa4b
Diegantos <diegantos8@gmail.com>
2023-07-25 12:18:36 +0200

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { UsersApp } from './UsersApp'
+import { LoginPage } from './auth/pages/LoginPage';
 
 function App() {
 
   return (
     <>
+      {/* <LoginPage /> */}
       <UsersApp />
     </>
   )
--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 
 export const LoginPage = () => {
   return (
-    <div className="modal" tabindex="-1">
+    <div className="modal" style={ {display: "block"} } tabIndex="-1">
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
@@ -9,8 +9,17 @@ export const LoginPage = () => {
           </div>
           <form>
             <div className="modal-body">
-                <input className="form-control my-3 w-75" placeholder="Username" name="username" />
-                <input className="form-control my-3 w-75" placeholder="Password" name="password" />
+              <input
+                className="form-control my-3 w-75"
+                placeholder="Username"
+                name="username"
+              />
+              <input
+                className="form-control my-3 w-75"
+                placeholder="Password"
+                type="password"
+                name="password"
+              />
             </div>
             <div className="modal-footer">
               <button type="submit" className="btn btn-primary">
