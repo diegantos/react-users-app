ad683365943686f157e377de0653505485f531a7
Diegantos <diegantos8@gmail.com>
2023-08-03 19:01:32 +0200

    routes ok

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,6 +1,4 @@
 import { LoginPage } from "./auth/pages/LoginPage";
-import { UsersPage } from "./pages/UsersPage";
-import { Navbar } from "./components/layout/Navbar";
 import { useAuth } from "./auth/hooks/useAuth";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { UserRoutes } from "./routes/UserRoutes";
@@ -11,24 +9,20 @@ export const UsersApp = () => {
 
   return (
     <Routes>
-      <>
-        {login.isAuth ? (
-          <Route
-            path="/*"
-            element={<UserRoutes />}
-            login={login}
-            handlerLogout={handlerLogout}
-          />
-        ) : (
-          <>
+      {login.isAuth ? (
+        <Route
+          path="/*"
+          element={<UserRoutes login={login} handlerLogout={handlerLogout} />}
+        />
+      ) : (
+        <>
           <Route
             path="/login"
             element={<LoginPage handlerLogin={handlerLogin} />}
           />
-          <Route path="/*" element={ <Navigate to='/login'/>}/>
-          </>
-        )}
-      </>
+          <Route path="/*" element={<Navigate to="/login" />} />
+        </>
+      )}
     </Routes>
   );
 };
