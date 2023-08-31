f16e2e3fcdbe5504b9bbeb936269257cd8bf7e87
Diegantos <diegantos8@gmail.com>
2023-08-31 18:23:45 +0200

    context

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -2,10 +2,12 @@ import { LoginPage } from "./auth/pages/LoginPage";
 import { useAuth } from "./auth/hooks/useAuth";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { UserRoutes } from "./routes/UserRoutes";
+import { useContext } from "react";
+import { AuthContext } from "./auth/context/AuthContext";
 
 export const UsersApp = () => {
-  
-  const { login, handlerLogin, handlerLogout } = useAuth();
+
+  const { login } = useContext(AuthContext)
 
   return (
     <Routes>
