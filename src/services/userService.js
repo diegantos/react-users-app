138890ebbab59fab9bce88ffff0bffe45dd803c0
Diegantos <diegantos8@gmail.com>
2023-09-06 19:44:59 +0200

    put

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,24 +2,37 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:8080/users";
 
-export const findAll = async (BASE_URL) => {
+export const findAll = async () => {
   try {
-    const response = await axios.get();
+    const response = await axios.get(BASE_URL);
     return response;
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
   return null;
 };
 
-export const save = async ({username, email, password}) => {
+export const save = async ({ username, email, password }) => {
+  try {
+    return axios.post(BASE_URL, {
+      username,
+      email,
+      password,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+  return undefined
+};
+
+export const update = ({ id, username, email }) => {
     try {
-        return axios.post(BASE_URL, {
+        return axios.put(`${BASE_URL}/${id}`, {
             username,
             email,
-            password,
         })
     } catch (error) {
-        
+        console.error(error)
     }
+    return undefined
 }
